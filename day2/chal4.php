093bfe297fc5600f6165b86bb26a71cf0f787d85
Rohan <rohanharris1@outlook.com>
2020-08-04 12:42:35 +0100

    chllenge 4 working

---
+++ day2/chal4.php
@@ -0,0 +1,16 @@
+<!-- Create a function phoneNumber, that returns true if the given string contains only numbers and hyphens:
+
+Hint: you can use both anchors (^ and $) in the same regex -->
+
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function phoneNumber($str){
+    return preg_match("/^[0-9-]+$/", $str) === 1;
+}
+
+dump(phoneNumber("blah")); // false
+dump(phoneNumber("12-323-5-344")); // true
+dump(phoneNumber("12-34-a334-43")); // false
+dump(phoneNumber("--123----23123")); // true
