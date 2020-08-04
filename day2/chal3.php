7f159987f9f5517d23ab12df02a375abd0dc07ee
Rohan <rohanharris1@outlook.com>
2020-08-04 12:23:28 +0100

    third challenge working

---
+++ day2/chal3.php
@@ -0,0 +1,14 @@
+<!-- Create a function, zOrQ, that returns true if the given string contains the letter q or z (uppercase and lowercase): -->
+
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function zOrQ($str){
+    return preg_match("/[qz]/i", $str) === 1;
+}
+
+dump(zOrQ("blah")); // false
+dump(zOrQ("blqh")); // true
+dump(zOrQ("bZ3h5")); // true
+dump(zOrQ("bz3h5")); // true
