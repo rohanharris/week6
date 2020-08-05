d230adf642ff46409a254314be6e3a8774fd5eaf
Rohan <rohanharris1@outlook.com>
2020-08-05 16:10:37 +0100

    recipe challenge started

--- day3/02_string.php
+++ day3/02_string.php
@@ -53,3 +53,5 @@ dump($string2->repeat(2)->upper()->append("!")->get()); // "SPOONSPOON!"
 
 $string3 = new StringyRedux("Na");
 dump($string3->repeat(2)->append(" ")->repeat(8)->append(" ")->append("Batman!")->get()); // "NaNa NaNa NaNa NaNa NaNa NaNa
+
+// if remove the second append("") it will get rid of the second space before "Batman!". str_repeat is repeating the string and the appended space 8 times not just the string which it would do in the for loop in the answer??
---
+++ day3/04_recipe.php
@@ -0,0 +1,74 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Recipe
+{
+    private $name;
+
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+
+    }
+
+
+
+}
+
+
+class Ingredient
+{
+    private $ingredient;
+    private $notes = [];
+
+    public function __construct($ingredient, $notes)
+    {
+        $this->ingredient = $ingredient;
+        $this->notes = $notes;
+    }
+
+}
+
+// ingredients take a name, followed by an array of dietary notes
+$flour = new Ingredient("Flour", ["gluten"]);
+$eggs = new Ingredient("Eggs", ["animal produce"]);
+$sugar = new Ingredient("Sugar", []);
+$butter = new Ingredient("Butter", ["dairy", "animal produce"]);
+
+// a recipe takes a name
+$cake = new Recipe("Cake");
+
+// we can add ingredients plus amounts
+$cake->addIngredient($flour, "200g");
+$cake->addIngredient($butter, "100g");
+$cake->addIngredient($sugar, "50g");
+$cake->addIngredient($eggs, 2);
+
+// we can add a method
+$cake->addMethod("Put them in a bowl, mix them together, cook for a bit. Job's a good'un");
+
+// we can see the recipe
+// using echo so it looks nicer
+echo $cake->display();
+/*
+    "Cake
+
+    Ingredients
+
+    - 200g Flour
+    - 100g Butter
+    - 50g Sugar
+    - 2 Eggs
+
+    Method
+
+    Put them in a bowl, mix them together, cook for a bit. Job's a good'un"
+ */
+
+// we can list dietary information
+dump($cake->dietary()); // "gluten, animal produce, dairy"
+
+// is the recipe vegan? (i.e. contains animal produce)
+dump($cake->vegan()); // false
